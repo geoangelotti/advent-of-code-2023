29280b6346bf477110af427a3e3d0d2ed4e318ec
Geo Angelopoulos <geo.angelotti@gmail.com>
2023-12-02 18:06:40 +0100

    skeleton

---
+++ go/day02/lib.go
@@ -0,0 +1,9 @@
+package day02
+
+func ProcessPart1(input string) string {
+	return ""
+}
+
+func ProcessPart2(input string) string {
+	return ""
+}
---
+++ go/day02/lib_test.go
@@ -0,0 +1,23 @@
+package day02_test
+
+import (
+	"day02"
+	"testing"
+)
+
+const INPUT = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+
+func TestProcessPart1(t *testing.T) {
+	expected := "8"
+	answer := day02.ProcessPart1(INPUT)
+	if answer != expected {
+		t.Errorf("Expected %s got %s", expected, answer)
+	}
+}
+
+func TestProcessPart2(t *testing.T) {
+}
