618536d20ec291a539d62a8d3e77db2a7966fb7f
Geo Angelopoulos <geo.angelotti@gmail.com>
2023-12-05 16:09:01 +0100

    part 2 solution

--- go/day02/lib.go
+++ go/day02/lib.go
@@ -43,5 +43,41 @@ func ProcessPart1(input string) string {
 }
 
 func ProcessPart2(input string) string {
-	return ""
+	var sum uint64
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		var red uint64 = 0
+		var green uint64 = 0
+		var blue uint64 = 0
+		gameLine := strings.Split(line, ":")
+		plays := strings.Split(gameLine[1], ";")
+		possible := true
+		for _, play := range plays {
+			cubes := strings.Split(play, ",")
+			appearances := map[string]uint64{
+				"red":   0,
+				"green": 0,
+				"blue":  0,
+			}
+			for _, cube := range cubes {
+				atoms := strings.Split(cube, " ")
+				number, _ := strconv.ParseUint(atoms[1], 10, 64)
+				colour := atoms[2]
+				appearances[colour] = number
+			}
+			if appearances["red"] > red {
+				red = appearances["red"]
+			}
+			if appearances["green"] > green {
+				green = appearances["green"]
+			}
+			if appearances["blue"] > blue {
+				blue = appearances["blue"]
+			}
+		}
+		if possible {
+			sum += red * green * blue
+		}
+	}
+	return fmt.Sprint(sum)
 }
