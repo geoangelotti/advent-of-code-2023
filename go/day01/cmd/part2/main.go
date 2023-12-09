e63788c7f2d0fb4774ff890f8a0cafbe95a75a8f
Geo Angelopoulos <14923436+geoangelotti@users.noreply.github.com>
2023-12-09 22:53:25 +0100

    Day 01/go (#1)
    * solution
    
    * part 2 naive solution
    
    ! not passing test :(
    
    * refactor part 1
    
    compare runes with chars

---
+++ go/day01/cmd/part2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"day01"
+	"fmt"
+	"os"
+)
+
+func main() {
+	content, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(day01.ProcessPart2(string(content)))
+}
---
+++ go/day01/lib.go
@@ -0,0 +1,48 @@
+package day01
+
+import (
+	"fmt"
+	"strings"
+)
+
+func ProcessPart1(input string) string {
+	lines := strings.Split(input, "\n")
+	var sum int64
+	for _, line := range lines {
+		values := []int64{}
+		for _, char := range line {
+			if '9' >= char && '0' <= char {
+				values = append(values, int64(char)-'0')
+			}
+		}
+		if len(values) > 0 {
+			sum += values[0]*10 + values[len(values)-1]*1
+		}
+	}
+	return fmt.Sprint(sum)
+}
+
+func ProcessPart2(input string) string {
+	letterNumbers := map[string]string{
+		"zero":  "z0ero",
+		"one":   "o1ne",
+		"two":   "t2wo",
+		"three": "t3hree",
+		"four":  "f4our",
+		"five":  "f5ive",
+		"six":   "s6ix",
+		"seven": "s7even",
+		"eight": "e8ight",
+		"nine":  "n9ine",
+	}
+	lines := strings.Split(input, "\n")
+	var sb strings.Builder
+	for _, line := range lines {
+		for k, v := range letterNumbers {
+			line = strings.Replace(line, k, v, -1)
+		}
+		sb.WriteString(line)
+		sb.WriteString("\n")
+	}
+	return ProcessPart1(sb.String())
+}
