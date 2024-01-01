b7ac44f5eba3b0acf00867e14cb444dfaae3e61f
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-01-01 14:13:23 +0200

    part 1 solution

--- rust/day_05/src/lib.rs
+++ rust/day_05/src/lib.rs
@@ -1,14 +1,113 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use nom::{
+    bytes::complete::take_until,
+    character::complete::{self, line_ending, space1},
+    multi::{many1, separated_list1},
+    sequence::tuple,
+    IResult, Parser,
+};
+use nom_supreme::{tag::complete::tag, ParserExt};
+use std::ops::Range;
+
+#[derive(Debug)]
+struct SeedMap {
+    mappings: Vec<(Range<u64>, Range<u64>)>,
+}
+
+impl SeedMap {
+    fn translate(&self, source: u64) -> u64 {
+        let valid_mapping = self
+            .mappings
+            .iter()
+            .find(|(source_range, _)| source_range.contains(&source));
+        dbg!(valid_mapping);
+        let Some((source_range, destination_range)) = valid_mapping else {
+            return source;
+        };
+        dbg!(source_range, destination_range);
+        let offset = source - source_range.start;
+
+        destination_range.start + offset
+    }
+}
+
+fn line(input: &str) -> IResult<&str, (Range<u64>, Range<u64>)> {
+    let (input, (destination, source, num)) = tuple((
+        complete::u64,
+        complete::u64.preceded_by(tag(" ")),
+        complete::u64.preceded_by(tag(" ")),
+    ))(input)?;
+    Ok((
+        input,
+        (source..(source + num), destination..(destination + num)),
+    ))
+}
+
+fn seed_map(input: &str) -> IResult<&str, SeedMap> {
+    take_until("map:")
+        .precedes(tag("map:"))
+        .precedes(many1(line_ending.precedes(line)).map(|mappings| SeedMap { mappings }))
+        .parse(input)
+}
+
+fn parse_seedmaps(input: &str) -> IResult<&str, (Vec<u64>, Vec<SeedMap>)> {
+    let (input, seeds) = tag("seeds: ")
+        .precedes(separated_list1(space1, complete::u64))
+        .parse(input)?;
+    let (input, maps) = many1(seed_map)(input)?;
+    Ok((input, (seeds, maps)))
+}
+
+pub fn process_part_1(input: &str) -> String {
+    let (_, (seeds, maps)) = parse_seedmaps(input).unwrap();
+    let locations = seeds
+        .iter()
+        .map(|seed| maps.iter().fold(*seed, |seed, map| map.translate(seed)))
+        .collect::<Vec<u64>>();
+    locations.iter().min().unwrap().to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INPUT: &str = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+";
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn part_1_works() {
+        let result = process_part_1(INPUT);
+        assert_eq!(result, 35.to_string());
     }
 }
