db3077609a67821633b38a5a45d4810aa13e71cb
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-01-10 23:12:05 +0100

    part 1 solution

--- clojure/day-04/src/day_04/core.clj
+++ clojure/day-04/src/day_04/core.clj
@@ -1,6 +1,34 @@
-(ns day-04.core)
+(ns day-04.core
+  (:require
+   [clojure.set :as set]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-sets [atoms]
+  (->> atoms
+       (map #(clojure.string/trim %))
+       (remove empty?)
+       (map #(Integer. %))
+       (set)))
+
+(defn extract-numbers [line]
+  (->> line
+       (#(clojure.string/split % #"\:"))
+       (last)
+       (#(clojure.string/split % #"\|"))
+       (map #(clojure.string/split % #" "))
+       (map get-sets)))
+
+(defn evaluate-score [winners]
+  (->> winners
+       (reduce clojure.set/intersection)
+       (count)
+       (#(if (= % 0) 0 (Math/pow 2 (dec %))))
+       (int)))
+
+(defn lines [input] (clojure.string/split-lines input))
+
+(defn process-part-1 [input]
+  (->> input
+       (lines)
+       (map extract-numbers)
+       (map evaluate-score)
+       (reduce +)))
