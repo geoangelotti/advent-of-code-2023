9b6f78e21864f5837f1fd3eeb88dd718f991a3cd
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-01-17 23:47:18 +0100

    part 1 test

---
+++ ocaml/day_07/test/test_day_07.ml
@@ -0,0 +1,15 @@
+open OUnit2
+open Day_07.Lib.Day_07
+
+let input = {|32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483|}
+
+let process_part_1_test _ =
+  let result = process_part_1 input in
+  assert_equal ~printer:(fun x -> x) "6440" result
+
+let suite = "Test Suite" >::: [ "process_part_1" >:: process_part_1_test ]
+let () = run_test_tt_main suite
