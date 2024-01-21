13992a5b18a71705d28d37843d6b28e6330c020d
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-01-21 20:35:06 +0100

    part 2 solution

--- rust/day_09/src/lib.rs
+++ rust/day_09/src/lib.rs
@@ -1,34 +1,53 @@
 use itertools::{Itertools, Position};
 
+fn process_history(mut nums: Vec<i64>) -> i64 {
+    let mut end_numbers: Vec<i64> = vec![];
+    loop {
+        if nums.iter().all(|n| n == &0) {
+            break;
+        }
+        nums = nums
+            .iter()
+            .tuple_windows::<(&i64, &i64)>()
+            .with_position()
+            .map(|(position, (left, right))| {
+                match position {
+                    Position::Last | Position::Only => {
+                        end_numbers.push(*right);
+                    }
+                    _ => {}
+                };
+                right - left
+            })
+            .collect::<Vec<i64>>();
+    }
+    end_numbers.iter().sum()
+}
+
 pub fn process_part_1(input: &str) -> String {
     input
         .lines()
         .map(|line| {
-            let mut nums = line
+            let nums = line
                 .split_whitespace()
                 .map(|num| num.parse::<i64>().unwrap())
                 .collect::<Vec<i64>>();
-            let mut end_numbers: Vec<i64> = vec![];
-            loop {
-                if nums.iter().all(|n| n == &0) {
-                    break;
-                }
-                nums = nums
-                    .iter()
-                    .tuple_windows::<(&i64, &i64)>()
-                    .with_position()
-                    .map(|(position, (left, right))| {
-                        match position {
-                            Position::Last | Position::Only => {
-                                end_numbers.push(*right);
-                            }
-                            _ => {}
-                        };
-                        right - left
-                    })
-                    .collect::<Vec<i64>>();
-            }
-            end_numbers.iter().sum::<i64>()
+            process_history(nums)
+        })
+        .sum::<i64>()
+        .to_string()
+}
+
+pub fn process_part_2(input: &str) -> String {
+    input
+        .lines()
+        .map(|line| {
+            let nums = line
+                .split_whitespace()
+                .map(|num| num.parse::<i64>().unwrap())
+                .rev()
+                .collect::<Vec<i64>>();
+            process_history(nums)
         })
         .sum::<i64>()
         .to_string()
@@ -46,5 +65,11 @@ mod tests {
     fn part_1_works() {
         let result = process_part_1(INPUT);
         assert_eq!(result, "114");
+    }
+
+    #[test]
+    fn part_2_works() {
+        let result = process_part_2(INPUT);
+        assert_eq!(result, "2");
     }
 }
