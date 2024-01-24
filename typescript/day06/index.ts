9c60b34ed8ef3fcbc1dffa2ec27070740977b170
Geo Angelopoulos <14923436+geoangelotti@users.noreply.github.com>
2024-01-24 13:17:44 +0100

    Day 06/typescript (#17)
    * bun empty project
    
    * part 1 solution
    
    * part 2 solution
    
    * change variable keyword

---
+++ typescript/day06/index.ts
@@ -0,0 +1,23 @@
+const zip = (a: RegExpMatchArray[], b:RegExpMatchArray[]) => a.map((k, i) => [k, b[i]]);
+
+export function processPart1(input: string): string {
+	const numbers = new RegExp(/[0-9]+/g);
+	const lines = input.split("\n");
+	const times = Array.from(lines[0].matchAll(numbers)); 
+	const distances = Array.from(lines[1].matchAll(numbers)); 
+	const zipped = zip(times, distances);
+	return zipped.map(item => {
+		const [[time], [distance]] = item.map(i => i.map(j => Number(j)));
+		const minimumTimeHeld = Array.from(Array(time+1).keys()).find(timeHeld => timeHeld * (time - timeHeld) > distance);
+		return time - 2 * Number(minimumTimeHeld) + 1
+	}).reduce((acc, c) => acc * c, 1).toString();
+};
+
+export function processPart2(input: string): string {
+	const numbers = new RegExp(/[0-9]+/g);
+	const lines = input.split("\n");
+	const time = Number(Array.from(lines[0].matchAll(numbers)).join("")); 
+	const distance = Number(Array.from(lines[1].matchAll(numbers)).join("")); 
+	const minimumTimeHeld = Array.from(Array(time+1).keys()).find(timeHeld => timeHeld * (time - timeHeld) > distance);
+	return (time - 2 * Number(minimumTimeHeld) + 1).toString();
+};
