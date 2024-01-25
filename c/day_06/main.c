82e45b8ca9783f68ff07dcd3db541418909c3b37
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-01-25 15:13:28 +0100

    part 2 solution

--- c/day_06/main.c
+++ c/day_06/main.c
@@ -54,8 +54,60 @@ long processpart1(char *filepath)
 	return product;
 }
 
+long processpart2(char *filepath)
+{
+	long product = 1;
+	long *values;
+	values = (long *)calloc(0, sizeof(long));
+	size_t size = 0;
+	long acc = 0;
+	int in = 0;
+	char buffer[1];
+	int f = open(filepath, O_RDONLY);
+	while (read(f, &buffer, 1) > 0)
+	{
+		if (buffer[0] >= '0' && buffer[0] <= '9')
+		{
+			if (in == 0)
+			{
+				in = 1;
+			}
+			acc = acc * 10 + buffer[0] - '0';
+		}
+		else if (buffer[0] == '\n')
+		{
+			if (in)
+			{
+				size += 1;
+				values = realloc(values, size * sizeof(long));
+				values[size - 1] = acc;
+				acc = 0;
+				in = 0;
+			}
+		}
+	}
+	close(f);
+	for (int i = 0; i < size / 2; i++)
+	{
+		long time = values[i];
+		long distance = values[size / 2 + i];
+		long timeheld = 0;
+		for (long j = 0; j <= time; j++)
+		{
+			timeheld = j;
+			if (timeheld * (time - timeheld) > distance)
+			{
+				break;
+			}
+		}
+		product *= time - 2 * timeheld + 1;
+	}
+	return product;
+}
+
 int main()
 {
 	printf("%ld\n", processpart1("input.txt"));
+	printf("%ld\n", processpart2("input.txt"));
 	return 0;
 }
