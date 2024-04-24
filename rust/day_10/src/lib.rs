d0b95b15222dee8d68d658b5fc6f463dd67b5e16
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-04-24 21:18:34 +0200

    part 1 test

--- rust/day_10/src/lib.rs
+++ rust/day_10/src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn process_part_1(input: &str) -> String {
+    input.into()
 }
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
+
     use super::*;
 
+    #[rstest]
+    #[case(
+        ".....
+.S-7.
+.|.|.
+.L-J.
+.....",
+        "4"
+    )]
+    #[case(
+        "..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...",
+        "4"
+    )]
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_process(#[case] input: &str, #[case] output: &str) {
+        let result = process_part_1(input);
+        assert_eq!(result, output);
     }
 }
